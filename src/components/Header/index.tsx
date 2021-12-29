07f3ebaf08f04d501eaa57c73ca9ee3ee8a111c6
Stefano Bacchini <stebacchini@gmail.com>
2021-12-29 16:18:05 -0300

    Create dashboard, and add axios and MirageJS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ export function Header() {
         Nova transação
 
       </button>
-      </Content>
+      </Content> 
     </Container>
   );
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+      api.get('transactions')
+      .then(data =>console.log(data))
+  }, []);
+
+
+  return (
+      <Container>
+          <table>
+              <thead>
+                  <tr>
+                     <th>Título</th> 
+                     <th>Valor</th> 
+                     <th>Categoria</th> 
+                     <th>Data</th> 
+                  </tr>
+              </thead>
+
+              <tbody>
+                  <tr>
+                      <td>Desenvolvimento de site</td>
+                      <td className="deposit">R$12.000</td>
+                      <td>Desenvolvimento</td>
+                      <td>29/12/2021</td>
+                  </tr>
+                  <tr>
+                      <td>Aluguel</td>
+                      <td className="withdraw">-R$1.100</td>
+                      <td>Casa</td>
+                      <td>17/12/2021</td>
+                  </tr>
+
+              </tbody>
+          </table>
+      </Container>
+  );
+}
